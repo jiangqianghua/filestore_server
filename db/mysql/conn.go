d3c2c68907c70cf2b38f7e5819f31460d918dc8c
jiangqianghua <240437339@qq.com>
2019-07-17 23:58:56 +0800

    nothing

--- db/mysql/conn.go
+++ db/mysql/conn.go
@@ -13,7 +13,7 @@ var db *sql.DB
 // 数据库初始化
 func init() {
 	fmt.Println("connect init...")
-	db, _ = sql.Open("mysql", "root:Jiang123!@tcp(180.76.105.202:3306)/filestore?charset=utf8")
+	db, _ = sql.Open("mysql", "root:aaaa@tcp(180.76.105.101:3306)/filestore?charset=utf8")
 	db.SetMaxOpenConns(1000)
 	err := db.Ping()
 	if err != nil {
